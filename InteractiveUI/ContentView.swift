a7c16d3a2a00fa5e9c87bd25809f90253b5e5b54
joan <nustellapham@gmail.com>
2025-08-06 14:53:55 -0400

    added @state property wrapper to create a functional button

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,17 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
     var body: some View {
-        VStack(spacing: 40.0) {
-            Text("What is your name?")
+        VStack(spacing: 35.0) {
+            Text(textTitle)
                 .font(.title)
-            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            TextField("Type your name here...", text: $name)
                 .font(.title2)
                 .padding(/*@START_MENU_TOKEN@*/.vertical, 15.0/*@END_MENU_TOKEN@*/)
                 .multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                 .border(Color.gray, width:1)
             Button("Submit name") {
-                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                textTitle = "Welcome \(name)!"
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
